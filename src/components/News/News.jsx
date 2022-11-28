d686c76622de52db79e7cd94589219aca5dfd8e7
sergiokano <sergiocano.design@gmail.com>
2022-11-28 16:36:53 +0100

    previous commit

---
+++ src/components/News/News.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { useContext } from "react";
+// import { GlobalContext } from "../context/GlobalState";
+import Menu from "../Menu"
+import NewsGrid from "../NewsGrid";
+
+
+const News = () => {
+    
+  return (
+    <>
+    <div>News</div>
+    <Menu/>
+    <NewsGrid/>
+    </>
+  )
+}
+
+export default News
