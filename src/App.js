2f9d7650f1bbe9aab54b715ee50d43f953ca5b19
sergiokano <sergiocano.design@gmail.com>
2022-11-28 16:47:05 +0100

    routes done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Menu from "./components/Menu";
-import NewsGrid from "./components/NewsGrid";
+import News from "./components/News/News";
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">See The Latest News</h1>
-      <GlobalProvider>
-        <Menu />
-        <NewsGrid />
-      </GlobalProvider>
+      <BrowserRouter>
+        <GlobalProvider>
+          <Routes>
+            <Route path="/" element={<News />} />
+          </Routes>
+        </GlobalProvider>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 // import { GlobalContext } from "../context/GlobalState";
 import Menu from "../Menu"
 import NewsGrid from "../NewsGrid";
-
+import "../../App.css";
 
 const News = () => {
     
   return (
     <>
-    <div>News</div>
+    <h1>News</h1>
     <Menu/>
     <NewsGrid/>
     </>
