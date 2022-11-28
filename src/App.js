e127143568ffb0855a61cfbe0b2171324dac1c3f
sergiokano <sergiocano.design@gmail.com>
2022-11-28 01:39:48 +0100

    categories done, api fetch correct, cards news done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Menu from "./components/Menu";
+import NewsGrid from "./components/NewsGrid";
+// import axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [active, setActive] = useState(1);
+  const [category, setCategory] = useState("general");
+
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=${category}&language=en&from=2022-10-28&sortBy=publishedAt&apiKey=04b6e0e527f84515b53d06f01a8efd4d`
+    )
+      .then((res) => res.json())
+      .then((data) => setItems(data.articles));
+  }, [category]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">See The Latest News</h1>
+      <Menu active={active} setActive={setActive} setCategory={setCategory} />
+      <NewsGrid items={items} />
     </div>
   );
 }
-
 export default App;
